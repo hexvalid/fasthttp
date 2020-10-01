0063e9960ee15d6fef6b64da62285e976f6224ae
hexvalid <hexvalid@gmail.com>
2020-10-01 14:59:31 +0300

    update for custom sorting

--- header.go
+++ header.go
@@ -1579,44 +1579,46 @@ func (h *RequestHeader) AppendBytes(dst []byte) []byte {
 	dst = append(dst, strHTTP11...)
 	dst = append(dst, strCRLF...)
 
-	userAgent := h.UserAgent()
-	if len(userAgent) > 0 {
-		dst = appendHeaderLine(dst, strUserAgent, userAgent)
-	}
+	if !h.disableNormalizing {
+		userAgent := h.UserAgent()
+		if len(userAgent) > 0 {
+			dst = appendHeaderLine(dst, strUserAgent, userAgent)
+		}
 
-	host := h.Host()
-	if len(host) > 0 {
-		dst = appendHeaderLine(dst, strHost, host)
-	}
+		host := h.Host()
+		if len(host) > 0 {
+			dst = appendHeaderLine(dst, strHost, host)
+		}
 
-	contentType := h.ContentType()
-	if len(contentType) == 0 && !h.ignoreBody() {
-		contentType = strPostArgsContentType
-	}
-	if len(contentType) > 0 {
-		dst = appendHeaderLine(dst, strContentType, contentType)
-	}
-	if len(h.contentLengthBytes) > 0 {
-		dst = appendHeaderLine(dst, strContentLength, h.contentLengthBytes)
+		contentType := h.ContentType()
+		if len(contentType) == 0 && !h.ignoreBody() {
+			contentType = strPostArgsContentType
+		}
+		if len(contentType) > 0 {
+			dst = appendHeaderLine(dst, strContentType, contentType)
+		}
+		if len(h.contentLengthBytes) > 0 {
+			dst = appendHeaderLine(dst, strContentLength, h.contentLengthBytes)
+		}
+
+		// there is no need in h.collectCookies() here, since if cookies aren't collected yet,
+		// they all are located in h.h.
+		n := len(h.cookies)
+		if n > 0 {
+			dst = append(dst, strCookie...)
+			dst = append(dst, strColonSpace...)
+			dst = appendRequestCookieBytes(dst, h.cookies)
+			dst = append(dst, strCRLF...)
+		}
+
+		if h.ConnectionClose() {
+			dst = appendHeaderLine(dst, strConnection, strClose)
+		}
 	}
 
 	for i, n := 0, len(h.h); i < n; i++ {
 		kv := &h.h[i]
 		dst = appendHeaderLine(dst, kv.key, kv.value)
-	}
-
-	// there is no need in h.collectCookies() here, since if cookies aren't collected yet,
-	// they all are located in h.h.
-	n := len(h.cookies)
-	if n > 0 {
-		dst = append(dst, strCookie...)
-		dst = append(dst, strColonSpace...)
-		dst = appendRequestCookieBytes(dst, h.cookies)
-		dst = append(dst, strCRLF...)
-	}
-
-	if h.ConnectionClose() {
-		dst = appendHeaderLine(dst, strConnection, strClose)
 	}
 
 	return append(dst, strCRLF...)
